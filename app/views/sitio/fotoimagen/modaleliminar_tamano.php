edca602c69731142f7d42c198887a0646f574f07
osmel <osmel_calderon@yahoo.com.ar>
2016-01-12 17:11:35 -0600

    los 2 que se agregaron

---
+++ app/views/sitio/fotoimagen/modaleliminar_tamano.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+	<div class="modal-header">
+		<a class="close" data-dismiss="modal">&times;</a>
+    	<div id="foo1"></div>
+    	<div class="alert" id="messages1"></div>
+
+
+		
+		
+	</div>
+	<div class="modal-body">
+
+ 			<div class="form-group">
+
+ 					<h5 class="text-center">Su eliminación es irreversible.</h5>
+            </div>      
+
+		<div class="alert" id="messagesModal"></div>
+	</div>
+	<div class="modal-footer">
+		<button class="btn btn-danger" name="nise" id="eliminar_diseno">Continuar</button>
+		<button class="btn btn-default" data-dismiss="modal">Regresar</button> 
+	</div>
